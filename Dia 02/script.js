4d603ac964c646c754876b9f8149038765f40ff0
Joana Amorim <138633905+joanasouzaa@users.noreply.github.com>
2024-04-23 10:47:19 -0300

    dia 2 - variáveis

---
+++ Dia 02/script.js
@@ -0,0 +1,8 @@
+const nomeUsuario = prompt("Qual o seu nome?");
+const idadeUsuario = Number(prompt("Quantos anos você tem?"));
+const linguagemProgUsuario = prompt("Qual linguagem de programaçao você está estudando?")
+
+const mensagemFinal = `Olá ${nomeUsuario}, você tem ${idadeUsuario} anos e já está aprendendo ${linguagemProgUsuario}!`
+
+alert(mensagemFinal)
+
